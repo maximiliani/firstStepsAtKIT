3efdb033a8b07fbbfde09801bcc9152d57fdee9a
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2021-09-22 11:06:30 +0200

    Added domain validation
    Fixed minor things like comments

--- mailAddressValidator/src/main/java/mail_address_validator/MailAddressValidator.java
+++ mailAddressValidator/src/main/java/mail_address_validator/MailAddressValidator.java
@@ -1,5 +1,7 @@
 package mail_address_validator;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,7 +16,7 @@ public class MailAddressValidator {
      *
      * @param args e-mail address to validate
      */
-    public static void main(String[] args) throws IllegalArgumentException {
+    public static void main(String[] args) throws IllegalArgumentException, UnknownHostException {
         System.out.println("This program validates only e-mail addresses without comments and static IP-addresses.");
         System.out.println();
 
@@ -26,7 +28,7 @@ public class MailAddressValidator {
         else{
             try{
                 String number = args[0];
-                validInput(number);
+                isValidInput(number);
                 System.out.println("This is a valid e-mail address!");
             } catch (Exception e) {
                 System.out.println("ERROR:   This e-mail address is invalid!");
@@ -40,19 +42,18 @@ public class MailAddressValidator {
     /**
      * This method validates e-mail addresses and throws an IllegalArgumentException if the address isn't valid.
      *
-     * @param inputAddress has to be a german national number without special characters or spaces
-     * @return true, if the number is valid
+     * @param inputAddress the mail address to check
+     * @return true, if the mail address is valid
      */
-    public static boolean validInput(String inputAddress) throws IllegalArgumentException {
+    public static boolean isValidInput(String inputAddress) throws IllegalArgumentException, UnknownHostException {
 
         if(inputAddress== null) throw new IllegalArgumentException();
-        /**
-         * This regex pattern was built based on the information about the german phone number system provided by the Bundesnetzagentur (Federal Grid Agency).
-         * It seperates the area code from the participants number by the position in the number.
-         * Emergency numbers like 110, 112 or other special short numbers are interpreted as an area code by this regex, so there is no participants number.
-         * In this case it must be verified that the area code matches one of these special numbers. This is done in the if-else-construct below.
-         */
 
+        /**
+         * This regex pattern was built based on the information provided on {@link https://www.mailboxvalidator.com/resources/articles/acceptable-email-address-syntax-rfc/#:~:text=The%20syntax%20for%20an%20email%20address%20is%20familiar,There%20specifications%20are%20called%20Requests%20For%20Comments%20%28RFC%29}.
+         * It splits the mail address in different parts like the local part (often username or name of the person who owns the mail address), the serverdomain, the SLD and the TLD.
+         * It also verifies that no unallowed characters are used.
+         */
         String regex = "^([A-Za-z0-9!#$%&'*+\\-/=?^_`{|}~][A-Za-z0-9.!#$%&'*+\\-/=?^_`{|}~]{0,62}[A-Za-z0-9!#$%&'*+\\-=?^_`|~]?)@((([A-Za-z0-9]+?[A-Za-z0-9-]{0,61}[A-Za-z0-9.])+)\\.([A-Za-z]+?[A-Za-z0-9+-]+))$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(inputAddress);
@@ -64,8 +65,28 @@ public class MailAddressValidator {
             System.out.println("Server domain without TLD: " + matcher.group(3));
             System.out.println("Second level domain (SLD): " + matcher.group(4));
             System.out.println("Top level domain (TLD): " + matcher.group(5));
-            if (inputAddress.length() == matcher.group(0).length() && matcher.group(0).length()<255) return true;
+            if (inputAddress.length() == matcher.group(0).length() && matcher.group(0).length()<255 && isValidDomain(matcher.group(2))) return true;
         }
         throw new IllegalArgumentException();
+    }
+
+    /**
+     * This method checks if A or AAAA records are available for the domain.
+     * @param inputDomain The domain of the server (the part after the '@')
+     * @return true if there are such records
+     * @throws UnknownHostException if there aren't such records
+     */
+    public static boolean isValidDomain(String inputDomain) throws UnknownHostException{
+        try{
+            InetAddress domain = InetAddress.getByName(inputDomain);
+            System.out.println("Hostname: " + domain.getHostName());
+            System.out.println("IP-Address: " + domain.getHostAddress());
+            return true;
+        } catch (UnknownHostException e){
+            System.out.println("ERROR:     No A or AAAA record available.");
+            System.out.println();
+            e.printStackTrace();
+            throw new UnknownHostException();
+        }
     }
 }
